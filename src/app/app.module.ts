2f79874d525998848554f3e6a9987e7054c8e7ee
Divas <divas.gulati@bupa.com.au>
2019-11-09 22:39:07 +1100

    userform changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { AppComponent } from './app.component';
-
-import {MatCheckboxModule} from '@angular/material/checkbox';
-
 import { userformComponent } from './components/userform/userform.component'
 import { userprofileComponent } from './components/userProfile/userprofile.component'
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,MatCheckboxModule ],
-  declarations: [ AppComponent,userformComponent,userprofileComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, MatButtonModule,
+    MatInputModule, MatCheckboxModule, MatSelectModule, ReactiveFormsModule],
+  declarations: [AppComponent, userformComponent, userprofileComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/userform/userform.component.ts
+++ src/app/components/userform/userform.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'userform',
   templateUrl: './userform.component.html',
   styleUrls: ['./userform.component.css']
 })
-export class userformComponent implements OnInit {
+export class userformComponent {
 
-  constructor() { }
+  email = new FormControl('', [Validators.required, Validators.email]);
 
-  ngOnInit() {
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
   }
 
 }
