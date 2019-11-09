f78268738dea4c0d2db2557e4f69f44d719e6d6d
Divas <divas.gulati@bupa.com.au>
2019-11-10 00:02:09 +1100

    ui design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {CdkTableModule} from '@angular/cdk/table';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 import { MatCheckboxModule } from '@angular/material/checkbox';
 
@@ -13,9 +17,9 @@ import { userformComponent } from './components/userform/userform.component'
 import { userprofileComponent } from './components/userProfile/userprofile.component'
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, MatButtonModule,
-    MatInputModule, MatCheckboxModule, MatSelectModule, ReactiveFormsModule],
-  declarations: [AppComponent, userformComponent, userprofileComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, MatButtonModule,MatCardModule,
+    MatInputModule, MatCheckboxModule, MatSelectModule, ReactiveFormsModule,CdkTableModule,MatTableModule,MatPaginatorModule],
+  declarations: [AppComponent, userformComponent, userprofileComponent,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/userProfile/userprofile.component.ts
+++ src/app/components/userProfile/userprofile.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'userprofile',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class userprofileComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['firstname', 'lastname', 'username', 'email','description','meal','department'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
   }
-
 }
+
+export interface PeriodicElement {
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  description: string;
+  meal: string;
+  department: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  //{position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {firstname: 'aa', lastname: 'Hydrogen',username: 'Hydrogen', email: 'Hydrogen', description: 'Hydrogen', meal: 'H',department: 'ss'},
+  {firstname: 'aa', lastname: 'Hydrogen',username: 'Hydrogen', email: 'Hydrogen', description: 'Hydrogen', meal: 'H',department: 'ss'},
+  {firstname: 'aa', lastname: 'Hydrogen',username: 'Hydrogen', email: 'Hydrogen', description: 'Hydrogen', meal: 'H',department: 'ss'},
+  {firstname: 'aa', lastname: 'Hydrogen',username: 'Hydrogen', email: 'Hydrogen', description: 'Hydrogen', meal: 'H',department: 'ss'},
+  {firstname: 'aa', lastname: 'Hydrogen',username: 'Hydrogen', email: 'Hydrogen', description: 'Hydrogen', meal: 'H',department: 'ss'},
+  {firstname: 'aa', lastname: 'Hydrogen',username: 'Hydrogen', email: 'Hydrogen', description: 'Hydrogen', meal: 'H',department: 'ss'}
+];
+
+
+/**  Copyright 2019 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
