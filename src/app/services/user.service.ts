10170afdf3b2ba9f1b2de7cb9885ab42d2ae1c8d
Divas <divas.gulati@bupa.com.au>
2019-11-10 20:24:40 +1100

    formating

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,17 +4,13 @@ import {Userinfo} from '../models/userinfo';
 import {HttpClient,HttpHeaders,HttpParams} from '@angular/common/http';
 import {tap} from 'rxjs/operators';
 
+const BASE_ADDRESS = 'https://zware-ngnewapi.azurewebsites.net';
+const key = 'gulati_divas_at_gmail_com';
+
 @Injectable()
 export class UserService {
 
   private _refreshNeeded$ = new Subject<void>();
-
-    //headers = new HttpHeaders()
-    //.set("Content-Type", "application/json").append();
-
-    // params = new HttpParams()
-    //.set('scopeKey', '"$key"')
-   
 
   get refreshNeeded$() {
     return this._refreshNeeded$;
@@ -23,13 +19,13 @@ export class UserService {
   constructor(private httpClient: HttpClient) { }
 
   getAllProfiles(): Observable<Userinfo[]> {
-    return this.httpClient.get<Userinfo[]>('https://zware-ngnewapi.azurewebsites.net/api/gulati_divas_at_gmail_com/profiles');
+    return this.httpClient.get<Userinfo[]>(`${BASE_ADDRESS}/api/${key}/profiles`);
   }
 
 
   saveUserInfo(userinfo: Userinfo): Observable<Userinfo> {
     return this.httpClient
-      .post<Userinfo>('https://zware-ngnewapi.azurewebsites.net/api/gulati_divas_at_gmail_com/profiles', userinfo)
+      .post<Userinfo>(`${BASE_ADDRESS}/api/${key}/profiles`, userinfo)
       .pipe(
         tap(() =>  {
           this._refreshNeeded$.next();
